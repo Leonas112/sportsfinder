aab8e556981ac594d52a6590d15d3f51e4823e21
Leonas112 <ixas12@gmail.com>
2025-06-17 13:07:40 +0300

    PostgreSQl config, cleanup of project

--- sportsfinder/settings.py
+++ sportsfinder/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'sportsfinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sportsfinder_db',
+        'USER': 'leonas',
+        'PASSWORD': 'monettaa123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
