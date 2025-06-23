1fb3271e14b6cd3c1801e9e085d6f265801f3363
Leonas Vedeikis <ixas12@gmail.com>
2025-06-23 22:08:02 +0300

    Initial CustomUser setup and Activity models

--- activities/models.py
+++ activities/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
 class Activity(models.Model):
     title       = models.CharField(max_length=120)
-    description = models.TextField(blank=True, null=True)
-    coach       = models.TextField()
-    price       = models.TextField()
-    location    = models.TextField(default="cool")
+    description = models.TextField()
+    category    = models.CharField(max_length=50)
+    contacts    = models.TextField()
+    location    = models.CharField(max_length=120)
+    owner       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
